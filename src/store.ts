393d959852599d4315f01ed7fe5098021fcf530a
bigeye <bigeye2599@gmail.com>
2025-01-03 12:36:13 +0900

    8-class2-part7-ch5-01 하는중

---
+++ src/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./slices/todoSlice";
+
+function createStore() {
+  const store = configureStore({
+    reducer: {
+      todo: todoReducer,
+    },
+  });
+
+  return store;
+}
+
+const store = createStore();
+
+export default store;
